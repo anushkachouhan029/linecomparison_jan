5ff1f9261d439a2e501f1e59d6384e6c1c068954
anushkachouhan029 <anushkachouhan029>
2023-01-09 23:09:37 +0530

    calculate line length

---
+++ calculate_line_length/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace LineComparisonProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Line Comparison Problems");
+
+            int x1, y1, x2, y2;
+
+            Console.Write("\nEnter x1 co-ordinate: ");
+            x1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter y1 co-ordinate: ");
+            y1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("\nEnter x2 co-ordinate: ");
+            x2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter y2 co-ordinate: ");
+            y2 = Convert.ToInt32(Console.ReadLine());
+
+            double lengthOfLine = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            Console.WriteLine($"\nObtained Length of Line is : " + lengthOfLine);
+        }
+    }
+}
